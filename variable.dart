2fe2b514f81d489dcb7c8527e9526e50b1fce470
sanaullah199 <sanau19191@gmail.com>
2025-06-19 04:40:36 -0700

    Add files via upload

---
+++ variable.dart
@@ -0,0 +1,19 @@
+//initialization mean defining variable DataType  space variableName  equalsign= and values Syntax:: bool myBoolean = true.
+//Declaration of variable mean that   define variables name and its type like Syntax:: DataType space variableName //  int myInteger;
+void main(){
+  int myValue = 10;                 //hold positive number 1,2,3...etc
+  double myFloat = 8.6;              //hold  floating points value like 3.4, 2.0, etc
+  String myCharacter = "welcome to my Account";     // used for abc or sentence , words
+  bool trueFalse = true;                        // use for condition check up true/ false 
+  List<String>myList = ["Apple "," Mango"," Banana" ]; // use to store data in organized forms or sequence 
+ // List<int>myList = [1,2,3,4,5 ];                     // this also use for keeping number type data
+  Map<String, int> myMap = {"Ali khan": 22,"Sudais malk":45}; //use to define unorder data mix data 
+// now i wanna show output 
+print("integer: $myValue");
+print("Double: $myFloat");
+print("String: $myCharacter");
+print("boolean: $trueFalse");
+print("List: $myList");
+print("List: $myList");
+print("Map: $myMap");
+}
